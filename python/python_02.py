1ae860c056083b6d74bfa364a573fc73f8cd9abd
thehapy0103@naver.com <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>
2023-12-06 16:44:34 +0900

    update

--- python/python_02.py
+++ python/python_02.py
@@ -3,3 +3,19 @@ sum = 0
 for num in range (5):
     sum += num
 print("~ 4 =", sum)
+sum = 0
+for num in range(11):
+    sum += num
+print("~ 10 =", sum)
+
+print("--------------------------")
+
+#calcsum
+def calcsum(n):
+    sum = 0
+    for num in range (n+1):
+        sum += num
+    return sum
+
+print("~ 4 =", calcsum(4))
+print("~ 10 =", calcsum(10))

db4710f5498ed1eff2dce3d2baa8279a1975ffcb
thehapy0103@naver.com <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>
2023-12-06 16:57:01 +0900

    update

--- python/python_03.py
+++ python/python_03.py
@@ -1,8 +1,8 @@
 #listslice
 nums = [0,1,2,3,4,5,6,7,8,9]
-print(nums[2:5]) #2���� 5����
-print(nums[:4]) #ó������ 4����
-print(nums[6:]) #6���� ������
-print(nums[2:5]) # 2�̻� 5 ����
-print(nums[1:7:2]) #1���� 7���� ��ĭ �ٱ�
+print(nums[2:5]) 
+print(nums[:4]) 
+print(nums[6:]) 
+print(nums[2:5]) 
+print(nums[1:7:2]) 
 
